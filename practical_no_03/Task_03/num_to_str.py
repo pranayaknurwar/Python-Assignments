c0aac04bc6b960f10f90ed49ccf0a078d4be38da
pranayaknurwar <aknurwarpranay@gmail.com>
2026-01-24 00:53:33 +0530

    Updation

--- practical_no_03/Task_03/num_to_str.py
+++ practical_no_03/Task_03/num_to_str.py
@@ -1,4 +1,7 @@
 n=int(input("Enter the nth number: "))
 
-for i in range(1,n+1):
-    print(f"{i}",end="")
+if 1<=n<=150:
+    for i in range(1,n+1):
+        print(f"{i}",end="")
+else:
+    print("Enter the number in range of 1 to 150.")
